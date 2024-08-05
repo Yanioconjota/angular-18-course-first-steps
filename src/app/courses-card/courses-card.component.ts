9f829eed5d27ae8943074fba05d32a3ea7b856fb
yanioconjota <janioisacura@gmail.com>
2024-08-05 17:01:27 -0300

    Example repo from course migrated

---
+++ src/app/courses-card/courses-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'courses-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './courses-card.component.html',
+  styleUrl: './courses-card.component.css'
+})
+export class CoursesCardComponent {
+
+}
